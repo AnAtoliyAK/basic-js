e33cf23b300b7c128dcf8c694f936f4dd496942d
AnAtoliy_AK <aliaksandrau89@gmail.com>
2020-03-16 21:01:27 +0200

    feat: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,6 +1,10 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
-
 module.exports = function dateSample(sampleActivity) {
-
+    if(typeof sampleActivity !== 'string') return false;
+  let sampleActivity1 = parseFloat(sampleActivity);
+ let time = (0.693 / HALF_LIFE_PERIOD);
+ let activity = Math.log(MODERN_ACTIVITY / sampleActivity1);
+ if(sampleActivity1 <= 0 || sampleActivity1 > MODERN_ACTIVITY || isNaN(sampleActivity1)) return false;
+ return Math.ceil(activity/time);
 };
