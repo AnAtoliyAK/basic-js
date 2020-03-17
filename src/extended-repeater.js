f0f4d3ece606ce937f2e1d4eadcd8fe6ca12b529
AnAtoliy_AK <aliaksandrau89@gmail.com>
2020-03-17 19:16:56 +0200

    feat: implement extended-repeater func

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,29 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+function inspectString(string, separator) {
+   return typeof(string) !== 'undefined' ? string : separator;
 };
-  
+
+module.exports = function repeater(str, options ) {
+   
+    const repeatTimes = options.repeatTimes || 0;
+    const separator = "" + inspectString(options.separator, '+');
+    const additionSeparator = "" + inspectString(options.additionSeparator, "|");
+    const addition = "" + inspectString(options.addition,"");
+    const additionRepeatTimes = options.additionRepeatTimes || 0;
+
+    str = "" + str;
+    let result = [];
+   
+    for (i = 0; i < repeatTimes ; i++){
+        let inArray = [];
+        for(j = 0 ; j < additionRepeatTimes ; j++){
+         inArray.push(addition);
+        }
+        if (inArray.length == 0 && addition != ""){
+         inArray.push(addition);
+        }
+
+        result.push (str + inArray.join(additionSeparator));
+    }
+    return result.length != 0 ? result.join(separator) : str + (addition != "" ? addition : "");
+
+}
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,26 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw Error;
+    }
+    let result =[];
+  if (arr.length === 0) {
+      return result;
+        }
+    for (let i=0; i<arr.length; i++) {
+        if (( i!= arr.length -1) && (arr[i] === '--discard-next')) {
+            i++;
+        }
+        else if (( i!= 0) && (arr[i] === '--discard-prev')) {
+            return result.splice(arr[i-1]);
+        }
+        else if (( i!= arr.length -1) && (arr[i] === '--double-next')) {
+            return result.push(arr[i+1]);
+        }
+        else if (( i!= 0) && (arr[i] === '--double-prev')) {
+            return result.push(arr[i-1]);
+        }
+        else result.push(arr[i]);
+      
+    }
+ return result;
 };
