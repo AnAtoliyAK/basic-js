b85c1c86be5384885f1e58d5069678826779546e
AnAtoliy_AK <aliaksandrau89@gmail.com>
2020-03-17 20:09:49 +0200

    feat: implement transform dunction

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,25 +2,27 @@ module.exports = function transform(arr) {
     if (!Array.isArray(arr)) {
         throw Error;
     }
-    let result =[];
-  if (arr.length === 0) {
-      return result;
-        }
-    for (let i=0; i<arr.length; i++) {
-        if (( i!= arr.length -1) && (arr[i] === '--discard-next')) {
-            i++;
-        }
-        else if (( i!= 0) && (arr[i] === '--discard-prev')) {
-            return result.splice(arr[i-1]);
-        }
-        else if (( i!= arr.length -1) && (arr[i] === '--double-next')) {
-            return result.push(arr[i+1]);
-        }
-        else if (( i!= 0) && (arr[i] === '--double-prev')) {
-            return result.push(arr[i-1]);
-        }
-        else result.push(arr[i]);
-      
+    let result = [];
+    if (arr.length === 0) {
+        return result;
     }
- return result;
+    arr.forEach((el, i, ar) => {
+        if (el !== '--discard-next' && el !== '--discard-prev' && el !== '--double-next' && el !== '--double-prev') {
+            result.push(el);
+            if ((ar[i - 1] === '--discard-next')) {
+                result.pop();
+            }
+            if (ar[i - 1] === '--double-next') {
+                result.push(el);
+            }
+            if (ar[i + 1] === '--discard-prev') {
+                result.pop();
+            }
+            if (ar[i + 1] === '--double-prev') {
+                result.push(el);
+            }
+        }
+    });
+
+    return result;
 };
