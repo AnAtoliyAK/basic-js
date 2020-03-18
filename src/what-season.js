832a5fd44f4f1e015d85092243f75a93849d5988
AnAtoliy_AK <aliaksandrau89@gmail.com>
2020-03-18 11:34:22 +0200

    feat: what seasons

--- src/what-season.js
+++ src/what-season.js
@@ -2,7 +2,9 @@ module.exports = function getSeason(date) {
   if (date === undefined) {
       return 'Unable to determine the time of year!';
   }
-
+ if ((!date instanceof Date) || (date instanceof Date && Object.getOwnPropertyNames(date).length !== 0)) {
+   throw new Error('');
+}
   const month = date.getMonth();
   if ((month < 2) || (month > 10)) {
     return 'winter';
